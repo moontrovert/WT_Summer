529bcd49f6d08392c323586f8c66e821ccb9e952
moontrovert <106690440+moontrovert@users.noreply.github.com>
2022-06-16 18:08:12 +0600

    Add files via upload

---
+++ TravelProject/view/traveler/travelerlogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1>Traveler Login Page</h1>
+    </head>
+    <body>
+        <table>
+            <form action = '', method = 'POST'>
+                <table>
+                    <tr>
+                        <td>Username:</td> 
+                        <td><input type = 'text' name = 'username'></td>
+                    </tr>
+                    <tr>
+                        <td>Password:</td>
+                        <td><input type='text' name='password' value =></td>
+                    </tr>
+                    <tr>
+                        <td><input type = 'submit' name = 'submit' value = 'SUBMIT'></button></td>
+                        <td><a href = 'travelerRegister.php'>Not registered yet?</a></td>
+                        <td><a href = '../mainpage.php'>Homepage</a></td>
+                    </tr>
+                </table>
+            </form>
+        </table>
+    </body>
+</html>
